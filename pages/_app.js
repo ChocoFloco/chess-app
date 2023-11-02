4cf8c6c396ba7c5996d8d4d1d136557f402e505e
ChocoFloco <149216961+ChocoFloco@users.noreply.github.com>
2023-11-02 13:38:34 -0500

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -4,7 +4,7 @@ function MyApp({ Component, pageProps }) {
   return (
     <div className="bg-cover" style={{backgroundImage: "url(/wallpaper.jpg)"}}>
     <div className="flex flex-row justify-center mt-4 mb-6">
-        <div className="font-serif text-7xl text-white">Chess App</div>
+        <div className="font-serif text-7xl text-white">Inverse Chess</div>
     </div>
     <Provider session={pageProps.session}>
       <Component {...pageProps} />
